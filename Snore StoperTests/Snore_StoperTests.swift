8a4ca6fcae584ea3a8386bf3d2ddd3125efa81c8
Hu Qiang <nickhuq@gmail.com>
2014-06-22 14:17:22 +0800

    Initial Commit

---
+++ Snore StoperTests/Snore_StoperTests.swift
@@ -0,0 +1,35 @@
+//
+//  Snore_StoperTests.swift
+//  Snore StoperTests
+//
+//  Created by Hu Qiang on 22/6/14.
+//  Copyright (c) 2014 Hu Qiang. All rights reserved.
+//
+
+import XCTest
+
+class Snore_StoperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
